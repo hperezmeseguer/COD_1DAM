9b68a55285280bcb4d9bee1ec0bbe7ba16032625
Hugo <hperezmeseguer@danielcastelao.org>
2025-12-02 13:39:40 +0100

    segunda funcion

--- Hundir la flota/barcos.py
+++ Hundir la flota/barcos.py
@@ -7,3 +7,6 @@ class Barcos:
     def recibir_impacto(self):
         if self.golpes_recibidos < self.longitud:
             self.golpes_recibidos += 1
+
+    def esta_hundido(self):
+        return self.golpess_recibidos >= self.longitud
