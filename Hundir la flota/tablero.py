90d90ae78b8933078f626a49c5c29cf75e01391b
Hugo <hperezmeseguer@danielcastelao.org>
2025-12-02 13:47:44 +0100

    añado filas y columnas

--- Hundir la flota/tablero.py
+++ Hundir la flota/tablero.py
@@ -4,3 +4,7 @@ class Tablero:
             self.dimensiones = (tamano, tamano)
         else:
             self.dimensiones = tamano
+
+        filas, columnas = self.dimensiones
+
+        self.casillas = [[0 for _ in range(columnas)] for _ in range(filas)]
